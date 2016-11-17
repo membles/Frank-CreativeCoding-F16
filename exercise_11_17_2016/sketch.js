79b1f6b4230e0326adb083a6e4429b604ac0f361
julian <julianfrank@live.com>
2016-11-17 09:55:09 -0500

    initial commit

---
+++ exercise_11_17_2016/sketch.js
@@ -0,0 +1,73 @@
+//Julian Frank
+//Creative Coding Fall 2016
+//Assignment 11.15
+//Visualizes the ratio of male to female artists who have work at the MOMA
+
+/* //part 1
+var myData;
+var aShape;
+var randIter;
+var randHex;
+var aColor;
+var shapes = [];
+
+function preload(){
+	var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/web_colors.json';
+	myData = loadJSON(url);
+}
+
+function setup(){
+	createCanvas(600,600);
+	noStroke();
+	randIter = int(random(0, myData.colors.length));
+	randHex = myData.colors[randIter].hex;
+	shapes[0] = new Shape(randHex, width/2, height/2);
+}
+
+function draw(){
+	if(mouseIsPressed){
+		randHex = myData.colors[int(random(0, myData.colors.length))].hex;
+		shapes[shapes.length] = new Shape(randHex, mouseX, mouseY);
+	}
+	for(var i = 0; i < shapes.length; i++){
+		shapes[i].display(randHex);
+	}
+}
+
+function Shape(hexVal, posX, posY){
+	this.hexVal = hexVal;
+	this.display = function(newHex){
+		fill(this.hexVal);
+		fill(newHex);
+		ellipse(posX,posY,70,70);
+	}
+};
+*/
+
+//pat 2
+var myData;
+var globalData;
+
+function setup(){
+	createCanvas(600,600);
+	var url = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/colors/web_colors.json';
+	myData = loadJSON(url, gotData);
+}
+
+function gotData(myData){
+	console.log(myData);
+	globalData = myData;
+}
+
+function draw(){
+	if(globalData){
+		//do a thing with the data
+	}
+	else{
+		//data isnt loaded yet; do something else
+	}
+}
+
+function mouseClicked(){
+
+}
