47f5d477c9d7842e69d7e5ea5b213d03b67db188
Julian Frank <memblesborough@gmail.com>
2016-11-28 01:21:43 -0500

    tweaks/comments

--- exercise_11_22_2016/sketch.js
+++ exercise_11_22_2016/sketch.js
@@ -2,6 +2,8 @@
 //Creative Coding Fall 2016
 //Exercise 11/22/2016
 
+//breaks on iterator == 1; reaches error when i click too frequently. switch to timed term change for testing and for main use.
+
 var baseURL, apiKey, url, searchTerm, globalData;
 var iterator;
 var myTerms = [];
@@ -11,12 +13,12 @@ function setup(){
 	iterator = 0;
 	createCanvas(600,600);
 	myTerms[0] = "tacos";
-	myTerms[1] = "cake";
+	myTerms[1] = "apples";
 	myTerms[2] = "fire%20extinguisher";
 	baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	apiKey = "63a778d9664948a2a5e59d994fbb454c";
-	searchTerm = "tacos";
-	url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[0];
+	//searchTerm = "tacos";
+	url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[1];
 	loadJSON(url, dataReceived);
 }
 
@@ -26,7 +28,9 @@ function draw(){
 		//console.log(globalData.response.docs[0].pub_date);
 		fill(255);
 		text(globalData.response.docs[0].pub_date, width/2, height/2);
+		console.log(globalData);
 	}
+	console.log(iterator);
 }
 
 function loadNewData(){
@@ -39,7 +43,7 @@ function dataReceived(incomingData){
 
 function mouseClicked(){
 	iterator++;
-	if(iterator > myTerms.length){
+	if(iterator >= myTerms.length){
 		iterator = 0;
 	}
 	url = baseURL + "?api-key=" + apiKey + "&q=" + myTerms[iterator];
