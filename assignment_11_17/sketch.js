e9a8cdf5c6f83248c598b458cd5c37b8e3690b99
julian <julianfrank@live.com>
2016-11-29 09:22:30 -0500

    works; temp and rain functionality

--- assignment_11_17/sketch.js
+++ assignment_11_17/sketch.js
@@ -4,7 +4,9 @@
 
 var baseURL, city, APIkey, url;
 var myData, globalData;
-var temp, tempMapped;
+var temp, tempMappedBlue, tempMappedRed;
+var weather;
+var raindrops = [];
 
 function setup(){
 	noStroke;
@@ -12,9 +14,16 @@ function setup(){
 	baseURL = "http://api.openweathermap.org/data/2.5/weather?";
 	city = "Newyork,ny";
 	APIkey = "3bc1861cff7eafa383ffe82135bc04fb";
-	url = baseURL + "q=" + city + "&callback=parseResponse&units=imperial&appid=" + APIkey;
+	url = baseURL + "q=" + city + "&units=imperial&appid=" + APIkey;
 	loadJSON(url, gotData);
-	setInterval(loadNewData, 4000);
+	setInterval(loadNewData, 30000);
+	textAlign(CENTER);
+	fill(255);
+	textSize(60);
+	textFont("Arial Black");
+	for(var i = 0; i < 30; i++){
+		raindrops[i] = new rain(random(5, 12));
+	}
 }
 
 function loadNewData(){
@@ -23,15 +32,38 @@ function loadNewData(){
 
 function gotData(incomingData){
 	globalData = incomingData;
-	println("test");
 }
 
 function draw(){
 	if(globalData){
-		console.log(globalData.parseResponse);
-		//console.log("test");
-		//temp = globalData.parseResponse.main.temp;
-		//tempMapped = map(temp, 0, 100, 255, 0);
-		//background(0,0,tempMapped);
+		console.log(globalData);
+		temp = globalData.main.temp;
+		tempMappedBlue = map(temp, 0, 100, 255, 0);
+		tempMappedRed = map(temp, 0, 100, 0, 255);
+		background(tempMappedRed,0,tempMappedBlue);
+		text(int(temp) + "°", width/2, height/2);
+
+		//weather functionality
+		weather = globalData.weather[0].main;
+		if(weather == "Rain"){
+			for(var i = 0; i < raindrops.length; i++){
+				raindrops[i].fall();
+			}
+		}
 	}
  }
+
+ function rain(spd){
+ 	this.xPos = random(0, width);
+ 	this.yPos = 0;
+ 	this.spd = spd;
+ 	this.fall = function(){
+ 		this.yPos += this.spd;
+ 		if(this.yPos > height){
+ 			this.yPos = 0;
+ 			this.xPos = random(0, width);
+ 		}
+ 		noStroke();
+ 		ellipse(this.xPos, this.yPos, 1, 5);
+ 	}
+ };
